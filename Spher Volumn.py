cf43733931f463f19fc85721022e32f4666fd1e9
sachinsaxena021988 <43206328+sachinsaxena021988@users.noreply.github.com>
2018-09-14 07:43:32 +0530

    Volume of sphere
    4. Write a Python program to find the volume of a sphere with diameter 12 cm.
    Formula: V=4/3 * π * r3

---
+++ Spher Volumn.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+# In[5]:
+
+
+# import math library to get the pi value 
+import math
+
+#define redius of the sphere
+r = 12
+
+# calculate the volume of the sphere
+
+V = 4/3 * math.pi * r**3
+
+#print the volume of the sphere
+
+print(V)
+
