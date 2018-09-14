f4b540a53dde2d37184a6d2a73119092fcef7cc2
sachinsaxena021988 <43206328+sachinsaxena021988@users.noreply.github.com>
2018-09-14 07:37:52 +0530

    Number problem
    Write a program which will find all such numbers which are divisible by 7 but are not a
    multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed
    in a comma-separated sequence on a single line.

---
+++ Number problem (2).py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# In[12]:
+
+
+#define number variable to save the value 
+number =''
+
+#loop through the collection range fron 2000 to 3200
+for value in range(2000,3200):
+    if value % 7 == 0 and value % 5 != 0:
+        #convert int to string 
+        number +=str(value) +','
+else:
+    #remove last comma frm the string
+    number = number[:-1]
+    #print the final output
+    print(number)
+
+                
+                
+
